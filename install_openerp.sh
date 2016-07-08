d52aa960bbf70855ecb80923036395d08b12c470
Diogo Cabral <diogo.silva@arapiraca.ufal.br>
2016-07-08 10:14:00 -0300

    Adicionando script de instalação

--- install_openerp.sh
+++ install_openerp.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 usuario=$USER
-echo "\n>>> Instalando Banco de dados\n"
+echo
+echo ">>> Instalando Banco de dados"
+echo
 sudo apt-get update
-sudo apt-get install postgresql -y
-echo "\n>>> Instalando Dependências do OpenERP\n"
+echo
+echo ">>> Instalando Dependências do OpenERP"
+echo
 sudo apt-get install -y graphviz ghostscript postgresql-client \
           python-dateutil python-feedparser python-gdata \
           python-ldap python-libxslt1 python-lxml python-mako \
@@ -13,13 +16,19 @@ sudo apt-get install -y graphviz ghostscript postgresql-client \
           python-werkzeug python-xlwt python-yaml python-imaging \
           python-matplotlib python-unittest2 python-mock python-docutils \
           python-decorator python-requests python-jinja2 python-pypdf python-passlib \
-          python-psutil python-pip python-setuptools
+          python-psutil python-pip python-setuptools postgresql git
 # iniciando a configuração
-echo "\n>>> Criando usuário para o banco de dados\n"
-sudo su postgres createuser $usuario -s -P $usuario
+echo
+echo ">>> Criando usuário para o banco de dados"
+echo
+sudo su postgres createuser $usuario -s
 cd /home/$usuario
-echo "\n>>> Clonando o repositório de código do OpenERP 7\n"
+echo
+echo ">>> Clonando o repositório de código do OpenERP 7"
+echo
 git clone https://github.com/ntiufalara/openerp7.git
-echo "\n>>> Executando o servidor e abrindo o navegador...\n"
+echo
+echo ">>> Executando o servidor e abrindo o navegador..."
+echo
 /home/$usuario/openerp7/openerp-server
 firefox "http://localhost:8069"
