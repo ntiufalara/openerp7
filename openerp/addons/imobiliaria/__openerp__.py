cea8c66dcb6efefdeb5c17d0a188f8c61b34c4eb
Diogo Cabral <diogocs1@gmail.com>
2016-07-17 18:13:39 -0300

    Removendo residuos do ud

---
+++ openerp/addons/imobiliaria/__openerp__.py
@@ -0,0 +1,17 @@
+# encoding: UTF-8
+
+{
+    'name': 'Imobiliária',
+    'description': '''
+    # Módulo base para imobiliária
+
+    Neste módulo está disponível o modelo básico para suporte de vendas e aluguéis de imóveis
+    ''',
+    'version': '1.0',
+    'depends': ['base'],
+    'data': [
+        'views/corretores_view.xml',
+        'views/menus.xml'
+    ],
+    'installable': True
+}
